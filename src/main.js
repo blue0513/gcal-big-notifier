9801c65fc1ac705cf20144235257c677a4ed1ec3
blue0513 <blue0513@users.noreply.github.com>
2021-09-04 15:15:20 +0000

    Prettified Code!

--- src/main.js
+++ src/main.js
@@ -5,63 +5,61 @@ var path = require("path");
 var mainWindow = null;
 var popupWindow = null;
 var createWindow = function () {
-    mainWindow = new electron_1.BrowserWindow({
-        width: 600,
-        height: 300,
-        frame: false,
-        hasShadow: false,
-        titleBarStyle: "customButtonsOnHover",
-        movable: true,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false
-        }
-    });
-    mainWindow.loadFile(path.join(__dirname, "../index.html"));
-    createPopupWindow();
-    // Open the DevTools.
-    // mainWindow.webContents.openDevTools()
+  mainWindow = new electron_1.BrowserWindow({
+    width: 600,
+    height: 300,
+    frame: false,
+    hasShadow: false,
+    titleBarStyle: "customButtonsOnHover",
+    movable: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+  mainWindow.loadFile(path.join(__dirname, "../index.html"));
+  createPopupWindow();
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
 };
 var createPopupWindow = function () {
-    popupWindow = new electron_1.BrowserWindow({
-        parent: mainWindow,
-        modal: true,
-        show: false,
-        width: 450,
-        height: 250,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false
-        }
-    });
-    popupWindow.loadFile(path.join(__dirname, "../popup.html"));
-    popupWindow.on("show", function () {
-        popupWindow.webContents.send("fromMain");
-    });
+  popupWindow = new electron_1.BrowserWindow({
+    parent: mainWindow,
+    modal: true,
+    show: false,
+    width: 450,
+    height: 250,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+  popupWindow.loadFile(path.join(__dirname, "../popup.html"));
+  popupWindow.on("show", function () {
+    popupWindow.webContents.send("fromMain");
+  });
 };
 electron_1.app.whenReady().then(function () {
-    createWindow();
-    electron_1.app.on("activate", function () {
-        if (electron_1.BrowserWindow.getAllWindows().length === 0)
-            createWindow();
-    });
+  createWindow();
+  electron_1.app.on("activate", function () {
+    if (electron_1.BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 });
 /* eslint-disable no-undef */
 electron_1.app.on("window-all-closed", function () {
-    if (process.platform !== "darwin")
-        electron_1.app.quit();
+  if (process.platform !== "darwin") electron_1.app.quit();
 });
 /* eslint-disable no-undef */
 electron_1.ipcMain.on("sendSchedule", function () {
-    mainWindow.restore();
-    mainWindow.setAlwaysOnTop(true);
+  mainWindow.restore();
+  mainWindow.setAlwaysOnTop(true);
 });
 electron_1.ipcMain.on("hideWindow", function () {
-    mainWindow.minimize();
+  mainWindow.minimize();
 });
 electron_1.ipcMain.on("showPopup", function () {
-    popupWindow.show();
+  popupWindow.show();
 });
 electron_1.ipcMain.on("hidePopup", function () {
-    popupWindow.hide();
+  popupWindow.hide();
 });
